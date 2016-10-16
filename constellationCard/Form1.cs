0f0c208d97f93544fb22f825a325f836d763455a
Michael Peterman <michaelpeterman@wightman.ca>
2016-10-16 15:48:13 -0400

    Added sounds

--- constellationCard/Form1.cs
+++ constellationCard/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Media;
 
 namespace constellationCard
 {
@@ -20,7 +21,7 @@ namespace constellationCard
 
         private void backgroundClick(object sender, MouseEventArgs e)
         {
-            //sets up everything needed to draw graphics
+            //sets up everything needed to draw graphics & play sounds
             Graphics formGraphics = this.CreateGraphics();
             Pen orangePen = new Pen(Color.Orange, 3);
             SolidBrush hotPinkBrush = new SolidBrush(Color.HotPink);
@@ -28,14 +29,17 @@ namespace constellationCard
             SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
             Font sansFont = new Font("Comic Sans MS", 9);
             Font sansLarge = new Font("Comic Sans MS", 15, FontStyle.Bold);
+            Font sansVeryLarge = new Font("Comic Sans MS", 20, FontStyle.Bold);
+            SoundPlayer songPlayer = new SoundPlayer(@"Resources\smilesAndTears.wav");
+            SoundPlayer fanfarePlayer = new SoundPlayer(@"Resources\fanfare.wav");
 
-            //wipes everything that was there and sets the background to white
+            //wipes everything that was there and sets the background to black
             titleLabel.Dispose();
             nameLabel.Dispose();
             dateLabel.Dispose();
             formGraphics.Clear(Color.Black);
 
-            //draws stars for constellations, with a half second wait before each start appears
+            //draws stars for the constellation, with a half second wait before each star appears
             Thread.Sleep(500);
             formGraphics.FillEllipse(yellowBrush, this.Width / 2 - 95, 155, 10, 10);
             Thread.Sleep(500);
@@ -49,10 +53,19 @@ namespace constellationCard
             formGraphics.DrawLine(orangePen, this.Width / 2 - 87, 107, this.Width / 2 + 75, 87);
             formGraphics.DrawLine(orangePen, this.Width / 2 + 75, 87, this.Width / 2 - 90, 160);
 
-            //re draws the "stars" so that they appear above the lines
+            //re draws the stars so that they appear above the lines
             formGraphics.FillEllipse(yellowBrush, this.Width / 2 - 95, 155, 10, 10);
             formGraphics.FillEllipse(yellowBrush, this.Width / 2 - 92, 102, 10, 10);
             formGraphics.FillEllipse(yellowBrush, this.Width / 2 + 70, 82, 10, 10);
+
+            //displays "TRIANGULUM" along the constellation
+            formGraphics.TranslateTransform(this.Width / 2 - 200, - 50);
+            formGraphics.RotateTransform(335);
+            formGraphics.DrawString("TRIANGULUM", sansVeryLarge, hotPinkBrush, 5, 250);
+            formGraphics.ResetTransform();
+
+            //plays the fanfare, pausing until it is complete
+            fanfarePlayer.PlaySync();
 
             //waits a second, then displays my text one line at a time with a .25 second pause inbetween each line
             Thread.Sleep(1000);
@@ -68,7 +81,10 @@ namespace constellationCard
             Thread.Sleep(250);
             formGraphics.DrawString("called it \"The Plow\", however this constellation", sansFont, whiteBrush, 10, 335);
             Thread.Sleep(250);
-            formGraphics.DrawString("contains another star called Gamma Andromodae.", sansFont, whiteBrush, 10, 350);
+            formGraphics.DrawString("contained another star called Gamma Andromodae.", sansFont, whiteBrush, 10, 350);
+
+            //plays Smiles and Tears
+            songPlayer.Play();
         }
     }
 }
